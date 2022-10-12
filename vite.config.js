c28ed8eae346e501e915b0debaed5e9386f5c292
JavGt <javier.bz.gt@gmail.com>
2022-10-12 11:01:04 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+	plugins: [react()],
+	server: {
+		host: true,
+		port: 3000,
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'@components': path.resolve(__dirname, 'src/components'),
+			'@pages': path.resolve(__dirname, 'src/pages'),
+			'@utils': path.resolve(__dirname, 'src/utils'),
+			'@assets': path.resolve(__dirname, 'src/assets'),
+		},
+	},
+});
